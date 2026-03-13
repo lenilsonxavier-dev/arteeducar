833d961b015c87802e05a8b82db38fab85cc6fef
lenilsonxavier-dev <lenilsonxavier@gmail.com>
2026-03-13 12:56:48 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,8 +28,14 @@ self.addEventListener('install', event => {
 // Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(cachedResponse => {
+        const fetchPromise = fetch(event.request).then(networkResponse => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+        return cachedResponse || fetchPromise;
+      });
     })
   );
 });
